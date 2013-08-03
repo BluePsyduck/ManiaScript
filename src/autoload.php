43a4c6d4141aeb9888bafc0dc778fcf2e871a38c
BluePsyduck <zwoetzen@yahoo.de>
2013-08-03 12:33:24 +0200

    Removed dump from autoload file.

--- src/autoload.php
+++ src/autoload.php
@@ -4,7 +4,6 @@ spl_autoload_register(function($class) {
     $found = false;
     if (substr($class, 0, 12) === 'ManiaScript\\') {
         $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-        var_dump($file);
         if (file_exists($file)) {
             require_once($file);
             $found = true;
